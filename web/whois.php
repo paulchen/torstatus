ee32eafc2a7cd76735fa140034df7f0e1ee0e328
Paul Staroch <paulchen@rueckgr.at>
2018-10-02 20:22:27 +0200

    fixed PHP error when there is no GET parameter named 'ip'

--- web/whois.php
+++ web/whois.php
@@ -3,7 +3,7 @@
 // Copyright (c) 2006-2007, Joseph B. Kowalski
 // See LICENSE for licensing information 
 
-$ip = $_GET['ip'];
+$ip = isset($_GET['ip']) ? $_GET['ip'] : '';
 if (!preg_match("/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/",$ip))
 {
 	header('HTTP/1.1 403 Forbidden');
