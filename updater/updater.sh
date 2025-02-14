99e3611d3335b67fab3fb50e03af9f0d47af23ac
Paul Staroch <paulchen@rueckgr.at>
2025-02-14 16:36:25 +0100

    add dependencies between containers and fix graceful shutdown

--- updater/updater.sh
+++ updater/updater.sh
@@ -5,16 +5,31 @@ SCRIPT="$DIRECTORY/tns_update.pl"
 
 cd "$DIRECTORY"
 
+rm -f .shutdown
+
+trap 'touch .shutdown ; killall perl' SIGTERM
+
 while true; do
 	ERROR=0
-	perl "$SCRIPT" || ERROR=1
+	perl "$SCRIPT" &
+	wait -f $! || ERROR=1
+
+	if [ -e .shutdown ]; then
+		break
+	fi
 
 	if [ "$ERROR" -eq 1 ]; then
 		echo "Script failed, waiting 15 seconds..."
-		sleep 15
+		sleep 15 &
+		wait $!
 	else
 		echo "Script successful, waiting 900 seconds..."
-		sleep 900
+		sleep 900 &
+		wait $!
+	fi
+
+	if [ -e .shutdown ]; then
+		break
 	fi
 done
 
